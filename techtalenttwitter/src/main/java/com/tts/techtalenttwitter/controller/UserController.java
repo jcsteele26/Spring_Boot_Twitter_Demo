898899609f947735039889aaedee8d5effcfd251
Andre <jcsteele26@gmail.com>
2020-07-29 15:19:38 -0400

    Added methods to Tweet Service

--- techtalenttwitter/src/main/java/com/tts/techtalenttwitter/controller/UserController.java
+++ techtalenttwitter/src/main/java/com/tts/techtalenttwitter/controller/UserController.java
@@ -24,21 +24,32 @@ public class UserController {
     private TweetService tweetService;
 
     @GetMapping(value = "/users/{username}")
-    public String getUser(@PathVariable(value="username") String username, Model model) {	
+    public String getUser(@PathVariable(value="username") String username, Model model) {
         
-    }
-
-    public String getUser(@PathVariable(value="username") String username, Model model) {	
+        User loggedInUser = userService.getLoggedInUser();
         User user = userService.findByUsername(username);
         List<Tweet> tweets = tweetService.findAllByUser(user);
+        List<User> following = loggedInUser.getFollowing();
+        boolean isFollowing = false;
+        for (User followedUser : following) {
+            if (followedUser.getUsername().equals(username)) {
+                isFollowing = true;
+            }
+        }
+        boolean isSelfPage = loggedInUser.getUsername().equals(username);
+        model.addAttribute("isSelfPage", isSelfPage);
+        model.addAttribute("following", isFollowing);
         model.addAttribute("tweetList", tweets);
         model.addAttribute("user", user);
         return "user";
     }
-    
+          
     @GetMapping(value = "/users")
     public String getUsers(Model model) {
         List<User> users = userService.findAll();
+        User loggedInUser = userService.getLoggedInUser();
+        List<User> usersFollowing = loggedInUser.getFollowing();
+        SetFollowingStatus(users, usersFollowing, model);
         model.addAttribute("users", users);
         SetTweetCounts(users, model);
         return "users";
@@ -52,5 +63,17 @@ public class UserController {
         }
         model.addAttribute("tweetCounts", tweetCounts);
     }
-            
-}
+
+    private void SetFollowingStatus(List<User> users, List<User> usersFollowing, Model model) {
+		HashMap<String, Boolean> followingStatus = new HashMap<>();
+		String username = userService.getLoggedInUser().getUsername();
+		for (User user : users) {
+			if (usersFollowing.contains(user)) {
+				followingStatus.put(user.getUsername(), true);
+			} else if (!user.getUsername().equals(username)) {
+				followingStatus.put(user.getUsername(), false);
+			}
+		}
+		model.addAttribute("followingStatus", followingStatus);
+	}
+}            
