43619974b76985b8cc63126e00aa24e4899480f2
Andre <jcsteele26@gmail.com>
2020-07-29 08:24:01 -0400

    Code for Twitter part one

---
+++ techtalenttwitter/src/main/java/com/tts/techtalenttwitter/service/UserService.java
@@ -0,0 +1,22 @@
+package com.tts.techtalenttwitter.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, 
+                       RoleRepository roleRepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+    
+}
