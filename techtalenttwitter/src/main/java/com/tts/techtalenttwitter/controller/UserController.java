6908eb48cea79570c9b61c1e2b3fe1e9a1cb2671
Andre <jcsteele26@gmail.com>
2020-07-30 10:18:36 -0400

    Update User Controller to filter messages

--- techtalenttwitter/src/main/java/com/tts/techtalenttwitter/controller/UserController.java
+++ techtalenttwitter/src/main/java/com/tts/techtalenttwitter/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.tts.techtalenttwitter.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -46,15 +48,33 @@ public class UserController {
     }
 
     @GetMapping(value = "/users")
-    public String getUsers(Model model) {
-        List<User> users = userService.findAll();
-        User loggedInUser = userService.getLoggedInUser();
-        List<User> usersFollowing = loggedInUser.getFollowing();
-        SetFollowingStatus(users, usersFollowing, model);
-        model.addAttribute("users", users);
-        SetTweetCounts(users, model);
-        return "users";
-    }
+	public String getUsers(@RequestParam(value = "filter", required = false) String filter, Model model) {
+		List<User> users = new ArrayList<User>();
+
+		User loggedInUser = userService.getLoggedInUser();
+
+		List<User> usersFollowing = loggedInUser.getFollowing();
+		List<User> usersFollowers = loggedInUser.getFollowers();
+		if (filter == null) {
+			filter = "all";
+		}
+		if (filter.equalsIgnoreCase("followers")) {
+			users = usersFollowers;
+			model.addAttribute("filter", "followers");
+		} else if (filter.equalsIgnoreCase("following")) {
+			users = usersFollowing;
+			model.addAttribute("filter", "following");
+		} else {
+			users = userService.findAll();
+			model.addAttribute("filter", "all");
+		}
+		model.addAttribute("users", users);
+
+		SetTweetCounts(users, model);
+		SetFollowingStatus(users, usersFollowing, model);
+
+		return "users";
+	}
 
     private void SetTweetCounts(List<User> users, Model model) {
         HashMap<String, Integer> tweetCounts = new HashMap<>();
